241b96688747b7166d6aba922b9a231c82d4817b
samirjabib <samir.jc123@gmail.com>
2022-09-14 09:25:38 -0500

    add add icon fecth to row

--- src/components/movie/movie.component.jsx
+++ src/components/movie/movie.component.jsx
@@ -7,17 +7,16 @@ const Movie = ({item}) => {
 
     const [like, setLike] = useState(true);
 
-    console.log(item)
  
     return (
 
-            <div className='w-[160px] sm:w-[200px] lg:w-[280px]  inline-block cursor-pointer relative p-2'>
+            <div className='w-[160px] sm:w-[200px] lg:w-[280px]  inline-block cursor-pointer relative p-2 '>
                 <img
-                    className ='w-full h-auto block'
+                    className ='w-full h-auto block '
                     src = {`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
                     alt = { item?.title }
                 />
-                <div className = 'absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+                <div className = 'absolute top-0 left-0 w-full h-full hover:bg-black/90 opacity-0 hover:opacity-100 text-white'>
                     <p className='flex justify-center items-center h-full '>{item?.title}</p>
                     <p >
                         {like ? (
--- src/components/row/row.component.jsx
+++ src/components/row/row.component.jsx
@@ -4,6 +4,9 @@ import {useState, useEffect} from 'react'
 
 import Movie from '../movie/movie.component'
 
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+
 const Row = ({ title, fetchURL }) => {
     const [movies, setMovies] = useState([])
 
@@ -17,12 +20,14 @@ const Row = ({ title, fetchURL }) => {
     return (
         <div>
             <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-            <div className="relative flex items-center">
-                <div id={'slider'}>
+            <div className="relative flex items-center group ">
+                <MdChevronLeft size={40} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'/>
+                <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative '>
                     {movies.map( (item, id) =>(
-                      <Movie item={ item } key={id}/>
+                      <Movie item={ item } key={id} />
                     ))}
                 </div>
+                <MdChevronRight size={40} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block '/>
             </div>
         </div>
     );
